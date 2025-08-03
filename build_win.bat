cbd6432ec8419908eb847da0242d5dba0364d42b
Ozar <116087247+Ozarchik@users.noreply.github.com>
2025-08-03 23:45:34 +0300

    update build bat file

--- build_win.bat
+++ build_win.bat
@@ -1,5 +1,7 @@
 @echo off
 
+@REM Example use: ./build_win.bat release mingw-release-17 
+
 set BUILD_TYPE_ARG=release
 set CONAN_PROFILE=default
 
@@ -15,27 +17,30 @@ echo Building with configuration: %BUILD_TYPE_ARG%
 echo Using Conan profile: %CONAN_PROFILE%
 echo.
 
-echo Running conan install...
+echo [1/3] Running conan install...
 conan install . -pr:a %CONAN_PROFILE% -b missing
-if !errorlevel! neq 0 (
+if %errorlevel% neq 0 (
     echo Error: conan install failed
-    exit /b !errorlevel!
+    exit /b %errorlevel%
 )
 
-echo Configuring CMake...
+echo [2/3] Configuring CMake...
 cmake --preset conan-%BUILD_TYPE_ARG%
-if !errorlevel! neq 0 (
+if %errorlevel% neq 0 (
     echo Error: cmake configure failed
-    exit /b !errorlevel!
+    exit /b %errorlevel%
 )
 
-echo Building project...
+echo [3/3] Building project...
 cmake --build --preset conan-%BUILD_TYPE_ARG%
-if !errorlevel! noq 0 (
+if %errorlevel% neq 0 (
     echo Error: cmake build failed
-    exit /b !errorlevel!
+    exit /b %errorlevel%
 )
 
-echo .
 echo "CGL-Graphics build done!"
+
+echo "Running app.."
+start "" "build\%BUILD_TYPE_ARG%\cglg.exe"
+
 exit /b 0
