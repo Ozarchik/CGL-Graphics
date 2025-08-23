f16bb6b9305ae8513e0432d0832c920f7b535ad8
Ozar <116087247+Ozarchik@users.noreply.github.com>
2025-08-23 18:38:22 +0300

    Changed main file

--- cgl/main.cpp
+++ cgl/main.cpp
@@ -1,10 +1,17 @@
 #include <cgl/application.h>
-
+#include <cgl/core/engine.h>
+#include <cgl/tools/backtrace/backtrace.h>
 
 int main(int argc, char* argv[])
 {
-    // CGL::Application app(argc, argv);
+    CGL::Backtrace::init();
+    CGL::ResourceManager::init();
+
+    CGL::Backtrace::instance().print();
+    CGL::CoreContext::instance();
+    CGL::Engine::instance();
     CGL::Application::instance().run();
+
     return 0;
 }
 
