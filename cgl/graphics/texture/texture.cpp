d5ba6da87a40ffc8183b1bd686fe4fef77aff482
Ozar <116087247+Ozarchik@users.noreply.github.com>
2025-08-26 19:44:59 +0300

    Added new static API in Texture class

--- cgl/graphics/texture/texture.cpp
+++ cgl/graphics/texture/texture.cpp
@@ -1,1 +1,25 @@
 #include <cgl/graphics/texture/texture.h>
+
+void CGL::Texture::bind2D(unsigned int id, int unit)
+{
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, id);
+}
+
+void CGL::Texture::unbind2D(int unit)
+{
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void CGL::Texture::filter2D(CGL::TextureFilter min, CGL::TextureFilter max)
+{
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, max);
+}
+
+void CGL::Texture::wrap2D(CGL::TextureWrap sWrap, CGL::TextureWrap tWrap)
+{
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, sWrap);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, tWrap);
+}
--- include/cgl/graphics/texture/texture.h
+++ include/cgl/graphics/texture/texture.h
@@ -2,6 +2,7 @@
 #define CGL_TEXTURE_H
 
 #include <string>
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 
 namespace CGL {
@@ -13,11 +14,24 @@ enum class TextureType {
     Height
 };
 
+enum TextureFilter {
+    Linear = GL_LINEAR
+};
+
+enum TextureWrap {
+    EdgeClamp = GL_CLAMP_TO_EDGE
+};
+
 struct Texture {
     unsigned int id = -1;
     TextureType type = TextureType::Diffuse;
     std::string path = "";
     glm::vec2 size;
+
+    static void bind2D(unsigned int id, int unit);
+    static void unbind2D(int unit);
+    static void filter2D(CGL::TextureFilter min, CGL::TextureFilter max);
+    static void wrap2D(CGL::TextureWrap sWrap, CGL::TextureWrap tWrap);
 };
 }
 
